bdd4762677780e672785092c698a3a3488657503
dav1312 <63931154+dav1312@users.noreply.github.com>
2023-05-04 11:44:50 +0200

    Changes - Change color of embeds - Change how the text of queries appears - Change how the title in the embed appears - Use marked to parse the markdown - Reformat code

--- commands/github.js
+++ commands/github.js
@@ -1,18 +1,18 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('github')
-        .setDescription('Retrieve Github commit')
-        .addStringOption(option =>
-            option
-                .setName('commit-sha')
-                .setDescription('Commit SHA')
-                .setRequired(true)
-        ),
-    async execute(interaction) {
-        const commitSha = interaction.options.getString('commit-sha');
-        const link = `https://github.com/official-stockfish/Stockfish/commit/${commitSha}`;
-        await interaction.reply(link);
-    },
+	data: new SlashCommandBuilder()
+		.setName('github')
+		.setDescription('Retrieve Github commit')
+		.addStringOption(option =>
+			option
+				.setName('commit-sha')
+				.setDescription('Commit SHA')
+				.setRequired(true),
+		),
+	async execute(interaction) {
+		const commitSha = interaction.options.getString('commit-sha');
+		const link = `https://github.com/official-stockfish/Stockfish/commit/${commitSha}`;
+		await interaction.reply(link);
+	},
 };
--- commands/wiki.js
+++ commands/wiki.js
@@ -1,88 +1,87 @@
-const fs = require("fs");
-const path = require("path");
-const { SlashCommandBuilder } = require("discord.js");
+const fs = require('fs');
+const path = require('path');
+const { SlashCommandBuilder } = require('discord.js');
+const marked = require('marked');
 
 // Read the markdown files in the Stockfish-wiki directory
-const wikiDirectory = path.join(__dirname, "..", "Stockfish.wiki");
+const wikiDirectory = path.join(__dirname, '..', 'Stockfish.wiki');
 const files = fs.readdirSync(wikiDirectory);
 
 // Create an object to store the subcommands
 const subcommands = {};
 
-const reduce = function (inputString) {
-    return inputString.replace(/[^\w- ]/g, '').replace(/\s/g, '-').toLowerCase();
+const reduce = (inputString) => {
+	return inputString.replace(/[^\w- ]/g, '').replace(/\s/g, '-').toLowerCase();
 };
 
 // Parse the markdown files to extract the headings
 files.forEach((file) => {
-  if (file.endsWith(".md")) {
-    const filePath = path.join(wikiDirectory, file);
-    const fileContent = fs.readFileSync(filePath, "utf8");
-    const headings = fileContent.match(/^#\s(.*)$/gm);
-    if (headings && headings.length > 0) {
-      const key = reduce(file.slice(0, file.length - 3));
-      subcommands[key] = headings.map((heading) => ({
-        name: reduce(heading.slice(2, heading.length)),
-        url:
-          `https://github.com/official-stockfish/Stockfish/wiki/${path.basename(
-            file,
-            ".md"
-          )}` +
-          "#" +
-          reduce(heading.slice(2, heading.length)),
-        value: heading.slice(2, heading.length).toLowerCase(),
-      }));
+	if (file.endsWith('.md')) {
+		const filePath = path.join(wikiDirectory, file);
+		const markdownContent = fs.readFileSync(filePath, 'utf-8');
+		const tokens = marked.lexer(markdownContent);
+		const headings = tokens.filter(token => token.type === 'heading' && token.depth === 1).map(token => token.text);
 
-      if (subcommands.length > 25) {
-        subcommands.length = 25;
-      }
-    }
-  }
+		if (headings && headings.length > 0) {
+			const key = reduce(file.slice(0, file.length - 3));
+			subcommands[key] = headings.map((heading) => ({
+				name: heading,
+				url: `https://github.com/official-stockfish/Stockfish/wiki/${path.basename(file, '.md')}#${reduce(heading)}`,
+				value: heading,
+				file: path.basename(file, '.md').replace(/-/g, ' '),
+			}));
+
+			if (subcommands.length > 25) {
+				subcommands.length = 25;
+			}
+		}
+	}
 });
 
 // Create the slash command data
 const commandData = new SlashCommandBuilder()
-  .setName("wiki")
-  .setDescription("Retrieve wiki articles");
+	.setName('wiki')
+	.setDescription('Retrieve wiki articles');
 
 // loop over keys
 for (const [key, value] of Object.entries(subcommands)) {
-  if (Object.keys(value).length > 25) {
-    continue;
-  }
-  commandData.addSubcommand((subcommand) =>
-    subcommand
-      .setName(key)
-      .setDescription(key)
-      .addStringOption((option) =>
-        option
-          .setName("query")
-          .setDescription("name")
-          .addChoices(...value)
-          .setRequired(true)
-      )
-  );
+	if (Object.keys(value).length > 25) {
+		continue;
+	}
+	commandData.addSubcommand((subcommand) =>
+		subcommand
+			.setName(key)
+			.setDescription(key)
+			.addStringOption((option) =>
+				option
+					.setName('query')
+					.setDescription('Choose a heading')
+					.addChoices(...value)
+					.setRequired(true),
+			),
+	);
 }
 
 // Export the slash command data and execute function
 module.exports = {
-  data: commandData,
-  async execute(interaction) {
-    const query = reduce(interaction.options.getString("query"));
-    const subs = subcommands[interaction.options.getSubcommand()];
-    let page = null;
+	data: commandData,
+	async execute(interaction) {
+		const query = interaction.options.getString('query');
+		const subs = subcommands[interaction.options.getSubcommand()];
+		let page = null;
 
-    for (const sub of subs) {
-      if (sub["name"] === query) {
-        page = sub;
-        break;
-      }
-    }
+		for (const sub of subs) {
+			if (sub['name'] === query) {
+				page = sub;
+				break;
+			}
+		}
 
-    const embed = {
-      title: page["name"],
-      url: page["url"],
-    };
-    await interaction.reply({ embeds: [embed] });
-  },
+		const embed = {
+			title: page['file'] + ': ' + page['name'],
+			url: page['url'],
+			color: parseInt('518047', 16),
+		};
+		await interaction.reply({ embeds: [embed] });
+	},
 };
