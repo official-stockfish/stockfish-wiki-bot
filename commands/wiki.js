1fde36f0833fba2c166b53dc82af827b88ac2d83
dav1312 <63931154+dav1312@users.noreply.github.com>
2023-05-05 13:31:25 +0200

    Allow requests without queries (#7)

--- commands/wiki.js
+++ commands/wiki.js
@@ -14,26 +14,25 @@ const reduce = (inputString) => {
 	return inputString.replace(/[^\w- ]/g, '').replace(/\s/g, '-').toLowerCase();
 };
 
+const baseUrl = 'https://github.com/official-stockfish/Stockfish/wiki/';
+
 // Parse the markdown files to extract the headings
 files.forEach((file) => {
-	if (file.endsWith('.md')) {
+	// Ignore files like _Footer.md or _Sidebar.md
+	if (file.endsWith('.md') && !file.startsWith('_')) {
 		const filePath = path.join(wikiDirectory, file);
+		const fileName = path.basename(file, '.md');
 		const markdownContent = fs.readFileSync(filePath, 'utf-8');
 		const tokens = marked.lexer(markdownContent);
 		const headings = tokens.filter(token => token.type === 'heading' && token.depth === 1).map(token => token.text);
 
-		if (headings && headings.length > 0) {
-			const key = reduce(file.slice(0, file.length - 3));
-			subcommands[key] = headings.map((heading) => ({
-				name: heading,
-				url: `https://github.com/official-stockfish/Stockfish/wiki/${path.basename(file, '.md')}#${reduce(heading)}`,
-				value: heading,
-				file: path.basename(file, '.md').replace(/-/g, ' '),
-			}));
+		subcommands[reduce(fileName)] = {
+			file: fileName,
+			queries: headings,
+		};
 
-			if (subcommands.length > 25) {
-				subcommands.length = 25;
-			}
+		if (subcommands.length > 25) {
+			subcommands.length = 25;
 		}
 	}
 });
@@ -45,43 +44,52 @@ const commandData = new SlashCommandBuilder()
 
 // loop over keys
 for (const [key, value] of Object.entries(subcommands)) {
+	const choices = value.queries.map(query => ({ name: query, value: query }));
 	if (Object.keys(value).length > 25) {
 		continue;
 	}
-	commandData.addSubcommand((subcommand) =>
-		subcommand
+	commandData.addSubcommand((subcommand) => {
+		const option = subcommand
 			.setName(key)
-			.setDescription(key)
-			.addStringOption((option) =>
-				option
+			.setDescription(key);
+
+		if (value.queries.length > 0) {
+			option.addStringOption((opt) =>
+				opt
 					.setName('query')
 					.setDescription('Choose a heading')
-					.addChoices(...value)
-					.setRequired(true),
-			),
-	);
+					.addChoices(...choices)
+					.setRequired(false),
+			);
+		}
+
+		return subcommand;
+	});
 }
 
 // Export the slash command data and execute function
 module.exports = {
 	data: commandData,
 	async execute(interaction) {
-		const query = interaction.options.getString('query');
-		const subs = subcommands[interaction.options.getSubcommand()];
-		let page = null;
+		const requestedQuery = interaction.options.getString('query');
+		const requestedSub = subcommands[interaction.options.getSubcommand()];
 
-		for (const sub of subs) {
-			if (sub['name'] === query) {
-				page = sub;
-				break;
+		const embed = {
+			title: requestedSub.file.replace(/-/g, ' '),
+			url: baseUrl + reduce(requestedSub.file),
+			color: parseInt('518047', 16),
+		};
+
+		if (requestedQuery) {
+			for (const subQuery of requestedSub['queries']) {
+				if (subQuery === requestedQuery) {
+					embed.title += ': ' + requestedQuery;
+					embed.url += '#' + reduce(requestedQuery);
+					break;
+				}
 			}
 		}
 
-		const embed = {
-			title: page['file'] + ': ' + page['name'],
-			url: page['url'],
-			color: parseInt('518047', 16),
-		};
 		await interaction.reply({ embeds: [embed] });
 	},
 };
