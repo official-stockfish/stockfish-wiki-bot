5021e4b7133f87a503361dc79c8bb1b55fbcd1d4
disservin <disservin.social@gmail.com>
2023-11-05 19:46:38 +0100

    Create fen.js

---
+++ commands/fen.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("fen")
+		.setDescription("Display a FEN visually.")
+		.addStringOption((option) =>
+			option.setName("fen").setDescription("Board FEN").setRequired(true)
+		),
+	async execute(interaction) {
+		const fen = interaction.options.getString("fen");
+
+		const stm = fen.split(" ")[1] === "w" ? "white" : "black";
+
+		const imageUrl = `https://lichess1.org/export/fen.gif?fen=${encodeURI(
+			fen
+		)}&color=${stm}`;
+
+		await interaction.reply(`\`\`\`${fen}\`\`\`[Image](${imageUrl})`);
+	},
+};
